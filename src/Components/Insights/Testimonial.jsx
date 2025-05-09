eaba1a22c1441ff88cd3d0f61f16ac767bbf29de
Pranav Jeyan V <pranavjeyan0@gmail.com>
2025-05-09 11:16:03 +0530

    Update Testimonial.jsx

--- src/Components/Insights/Testimonial.jsx
+++ src/Components/Insights/Testimonial.jsx
@@ -82,6 +82,13 @@ const Testimonial = () => {
     autoplaySpeed: 3000,
     responsive: [
       {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
