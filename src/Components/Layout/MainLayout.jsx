9613d42046cfbb8cc4ab020080627c33f6cf4a9c
Pranav Jeyan V <pranavjeyan0@gmail.com>
2025-05-12 12:40:33 +0530

    Update MainLayout.jsx

--- src/Components/Layout/MainLayout.jsx
+++ src/Components/Layout/MainLayout.jsx
@@ -8,19 +8,6 @@ import Custom_Navbar from '../Custom_Navbar/Custom_Navbar'
 const MainLayout = () => {
   return (
     <>
-      <AnimatedCursor
-        innerSize={0} // Set innerSize to 0 to completely remove the inner cursor point
-        outerSize={35}
-        innerScale={0} // Ensure no scaling for the inner cursor
-        outerScale={1.7}
-        outerAlpha={0}
-        hasBlendMode={true}
-        outerStyle={{
-          backgroundImage: 'url(/public/cursor.png)',
-          backgroundSize: 'cover',
-          cursor: 'none'
-        }}
-      />
       <Custom_Navbar />
       <ScrollToTop />
       <Outlet />
