021cab25054d1631f255e2a66c5c0a4c5136dca2
Pranav Jeyan V <pranavjeyan0@gmail.com>
2025-05-12 13:14:57 +0530

    Update MainLayout.jsx

--- src/Components/Layout/MainLayout.jsx
+++ src/Components/Layout/MainLayout.jsx
@@ -4,10 +4,24 @@ import { Outlet } from 'react-router-dom'
 import ScrollToTop from './ScrollToTop'
 import Navbar from '../Navbar/Navbar'
 import Custom_Navbar from '../Custom_Navbar/Custom_Navbar'
+import AnimatedCursor from 'react-animated-cursor';
 
 const MainLayout = () => {
   return (
     <>
+      <AnimatedCursor
+        innerSize={0}
+        outerSize={35}
+        innerScale={0}
+        outerScale={1.7}
+        outerAlpha={0}
+        hasBlendMode={true}
+        outerStyle={{
+          backgroundImage: 'url(/public/cursor.png)',
+          backgroundSize: 'cover',
+          cursor: 'none'
+        }}
+      />
       <Custom_Navbar />
       <ScrollToTop />
       <Outlet />
