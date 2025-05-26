0142a1de90797c1dc62505ba72e12760194fedb2
Pranav Jeyan V <pranavjeyan0@gmail.com>
2025-05-26 16:02:39 +0530

    Update TechSolution.jsx

--- src/Components/Service/TechSolution.jsx
+++ src/Components/Service/TechSolution.jsx
@@ -130,8 +130,8 @@ const handleMouseLeave = (index) => {
                     <h1 className="text-[#19BDE8] font-semibold text-xl md:text-6xl leading-tight">
                         Technology Solutions
                     </h1>
-                    <p className="font-normal text-base  md:text-xl mt-6">
-                        We don’t just build tech — we craft intelligent, scalable solutions that power your business growth. From stunning websites and intuitive apps to emerging technologies like blockchain and AI, we turn ideas into digital excellence.
+                    <p className="font-normal text-base md:text-xl text-justify mt-6">
+                        We don't just build tech — we craft intelligent, scalable solutions that power your business growth. From stunning websites and intuitive apps to emerging technologies like blockchain and AI, we turn ideas into digital excellence.
                     </p>
                     <a href="#">
                         <CustomBtn> Power Your Business with Innovation</CustomBtn>
@@ -168,7 +168,7 @@ const handleMouseLeave = (index) => {
                                         {index + 1}
                                     </div>
                                     <h3 className="font-semibold text-xl md:text-2xl mb-4 text-black">{problem.title}</h3>
-                                    <p className="text-base font-normal text-black">{problem.description}</p>
+                                    <p className="text-base font-normal text-black text-justify">{problem.description}</p>
                                 </div>
                             ))}
                         </div>
@@ -189,7 +189,7 @@ const handleMouseLeave = (index) => {
                                         <img className='h-15 w-15' src={solution.image} alt="" />
                                         <div>
                                             <h4 className="font-medium text-lg md:text-2xl">{solution.title}</h4>
-                                            <p className="text-base text-gray-600 mt-1">
+                                            <p className="text-base text-gray-600 mt-1 text-justify">
                                                 {solution.description}
                                             </p>
                                         </div>
@@ -236,7 +236,7 @@ const handleMouseLeave = (index) => {
                     <h1 className='font-semibold text-xl lg:text-3xl md:text-2xl  mb-5'>
                         Explore More Ways We Can Help You Grow
                     </h1>
-                    <p className='text-base lg:text-sm md:text-xs'>
+                    <p className='text-base lg:text-sm md:text-xs text-justify'>
                         Your business needs more than just Multimedia. Discover how our full suite of services works together to drive real results.
                     </p>
                 </div>
