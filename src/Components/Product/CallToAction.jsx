0f2c3f9c30fd186698b9c8a69ba8359f4e19a168
Pranav Jeyan V <pranavjeyan0@gmail.com>
2025-06-26 12:22:26 +0530

    Update CallToAction.jsx

--- src/Components/Product/CallToAction.jsx
+++ src/Components/Product/CallToAction.jsx
@@ -7,9 +7,9 @@ const CallToAction = () => {
   <p className="text-gray-700 mb-6">
     Discover how MABS can transform your operations with cutting-edge solutions tailored to your needs.
   </p>
-  <button className="bg-black text-white px-6 py-2 rounded hover:bg-gray-800 transition cursor-pointer">
-    Contact Us
-  </button>
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '12px', marginTop: '20px' }}>
+            <button className="ui-btn"><span>Contact Us</span></button>
+         </div>
 </div>
 
   )
